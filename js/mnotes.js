90fda83983bd93b174f8261be04c1f7d59f385f5
Ara <info@app4brides.com>
2012-11-28 09:22:40 +0100

    try

---
+++ js/mnotes.js
@@ -0,0 +1,22 @@
+﻿function storeUserScribble(id) {
+  var scribble = document.getElementById('scribble').innerHTML;
+  localStorage.setItem('userScribble',scribble);
+}
+
+function getUserScribble() {
+  if ( localStorage.getItem('userScribble')) {
+    var scribble = localStorage.getItem('userScribble');
+  }
+  else {
+    var scribble = 'Write Here';
+  }
+  document.getElementById('scribble').innerHTML = scribble;
+}
+
+function clearLocal() {
+  clear: localStorage.clear(); 
+  return false;
+}
+
+
+
